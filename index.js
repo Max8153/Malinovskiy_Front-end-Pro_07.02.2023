6da7ef64d6df2dd4ef071fe4aa1e0b4f27dd918b
unknown <malinovskiy925@gmail.com>
2023-02-15 10:33:28 +0200

    hw3 done

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-let result = prompt("Who are you?");
-alert(`Hello, ${result}! How are you?`);
+let firstString = prompt("Enter first string");
+let secondtString = prompt("Enter second string");
+let thirdString = prompt("Enter third string");
+alert(firstString.concat(secondtString).concat(thirdString));
+
+const num = 12345;
+let arrey = ("" + num).split("");
+let result = arrey.join(" ");
+alert(result);
