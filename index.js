1500b237eac1d697080276bb16dac0c2afabc4ca
unknown <malinovskiy925@gmail.com>
2023-02-23 23:55:40 +0200

    hw6 done

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-let questionResult = prompt("Add, subtract, multiply or divide?");
-let firstNumber = prompt("Enter first number", 0);
-let secondNumber = prompt("Enter second number", 0);
-let firstResult = +firstNumber + +secondNumber;
-let secontResult = firstNumber - secondNumber;
-let thirdResult = firstNumber * secondNumber;
-let fourthResult = firstNumber / secondNumber;
-if (questionResult === "add") {
-    alert(firstResult);
-} else if (questionResult === "subtract") {
-    alert(secondResult);
-} else if (questionResult === "multiply") {
-    alert(thirdResult);
-} else if (questionResult === "divide") {
-    alert(fourthResult);
-} else {
-    alert("You have to choose only add, subtract, multiply or divide");
-}
+let questionResult = prompt("Tell me any number of hours");
+let result = questionResult * 60 * 60;
+alert("There are " + result + " seconds in " + questionResult + " hours!");
