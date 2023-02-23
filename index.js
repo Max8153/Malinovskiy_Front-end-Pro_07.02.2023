dde73bfcbacf8a5db4ea020e237690da06258ec5
unknown <malinovskiy925@gmail.com>
2023-02-23 23:57:23 +0200

    hw7 done

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-let questionResult = prompt("Tell me any number of hours");
-let result = questionResult * 60 * 60;
-alert("There are " + result + " seconds in " + questionResult + " hours!");
+let firstNumber = prompt("Enter first number");
+let secondNumber = prompt("Enter second number");
+let thirdNumber = prompt("Enter third number");
+let result = (+firstNumber + +secondNumber + +thirdNumber) / 3;
+alert("The arithmetic mean of these numbers is " + result);
