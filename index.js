09ffb4662a87c0365b00d5922d5b4ec405baa9ae
unknown <malinovskiy925@gmail.com>
2023-02-25 22:21:34 +0200

    hw5 modified

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 let questionResult = prompt("Add, subtract, multiply or divide?");
 let firstNumber = prompt("Enter first number", 0);
 let secondNumber = prompt("Enter second number", 0);
-let firstResult = +firstNumber + +secondNumber;
-let secontResult = firstNumber - secondNumber;
-let thirdResult = firstNumber * secondNumber;
-let fourthResult = firstNumber / secondNumber;
-if (questionResult === "add") {
-    alert(firstResult);
-} else if (questionResult === "subtract") {
-    alert(secondResult);
-} else if (questionResult === "multiply") {
-    alert(thirdResult);
-} else if (questionResult === "divide") {
-    alert(fourthResult);
-} else {
-    alert("You have to choose only add, subtract, multiply or divide");
+let addResult = +firstNumber + +secondNumber;
+let subResult = firstNumber - secondNumber;
+let mulResult = firstNumber * secondNumber;
+let divResult = firstNumber / secondNumber;
+
+switch (questionResult) {
+    case "add": alert(addResult);
+    break;
+    case "subtract": alert(subResult);
+    break;
+    case "multiply": alert(mulResult);
+    break;
+    case "divide": alert(divResult);
+    break;
+    default: alert("You have to choose only add, subtract, multiply or divide");
 }
