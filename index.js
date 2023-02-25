9283ac9b5762f02de68f02be852d5fb225bb0e1b
unknown <malinovskiy925@gmail.com>
2023-02-25 22:27:24 +0200

    hw7 modified

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstNumber = prompt("Enter first number");
-let secondNumber = prompt("Enter second number");
-let thirdNumber = prompt("Enter third number");
-let result = (+firstNumber + +secondNumber + +thirdNumber) / 3;
-alert("The arithmetic mean of these numbers is " + result);
+let firstNumber = Number(prompt('Write first number'));
+let secondNumber = Number(prompt('Write second number'));
+let thirdNumber = Number(prompt('Write third number'));
+let result = (firstNumber + secondNumber + thirdNumber) / 3;
+alert('The arithmetic mean of these numbers is ' + result);
